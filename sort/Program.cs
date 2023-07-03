bad041622709cbca227dbda994818ab2fbf18e30
Will <will@wiri.co.uk>
2023-07-03 11:33:24 +0100

    getting to grips with classes

--- sort/Program.cs
+++ sort/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+/*
+
+init randomised list of ints
+display ints current state
+display ints start state
+sort ints
+
+*/
+
+public class Integers
+{
+    public int[] members = new int[] {1, 3, 5, 2, 4, 9, 8, 6, 7};
+
+    public void Show()
+    {
+        Console.WriteLine(String.Join(" ", members));
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Integers a = new Integers();
+ 
+        a.Show();
+
+
+    }
+}
