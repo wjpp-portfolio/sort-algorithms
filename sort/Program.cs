dfc7ba92e93acf752f2f466e6c5d44fc81dd727d
Will <will@wiri.co.uk>
2023-07-05 09:22:12 +0100

    implimented merge sort

--- sort/Program.cs
+++ sort/Program.cs
@@ -1,37 +1,19 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     public static void Main()
     {
-        int[] numbers = new int[] {7,6,5,4,3,2,1};
+        List<int> numbers = new List<int> {8,3,2,9,4,6,5,7,1};
 
-        var a = new Sorter(numbers);
-        a.ShowUnSorted();
-        a.Bubble();
+        var a = new Sorter();
+
+        Display(numbers);
+        var result = a.Merge(numbers);
+        Display(result);
     
     }
-}
-
-class Sorter
-{
-    public int[] Sorted {get; }
-    public int[] UnSorted {get; set; }
-
-    public Sorter(int[] unsorted)
-    {
-        this.UnSorted = unsorted;
-    }
-    public void ShowSorted()
-    {
-        Display(Sorted);
-    }
-    public void ShowUnSorted()
-    {
-        Display(UnSorted);
-    }
-    private void Display(int[] listOfInts)
+    private static void Display(List<int> listOfInts)
     { 
         foreach (var item in listOfInts)
         {
@@ -39,26 +21,29 @@ class Sorter
         }
         Console.WriteLine("");
     }
-    public int[] Bubble()
+}
+
+class Sorter
+{
+    public List<int> Bubble(List<int> working_list)
     {
-        int[] i = this.UnSorted;
         int counter = 0;
         bool changes;
 
         do
         {
             changes = false;
-            for (int index = 0; index < i.Length - counter; index++)
+            for (int index = 0; index < working_list.Count - counter; index++)
             {             
                 try
                 {
-                    if (i[index] > i[index + 1])
+                    if (working_list[index] > working_list[index + 1])
                     {
-                        (i[index], i[index + 1]) = (i[index + 1], i[index]);
+                        (working_list[index], working_list[index + 1]) = (working_list[index + 1], working_list[index]);
                         changes = true;
                     }
                 }
-                catch (IndexOutOfRangeException)
+                catch (ArgumentOutOfRangeException)
                 {
                     //Console.WriteLine("---Index Out Of Range");
                 }               
@@ -68,7 +53,61 @@ class Sorter
  
         } while (changes != false);
 
-        Display(i);  
-        return i;     
+        return working_list;
+    }
+    public List<int> Merge(List<int> working_list)
+    {
+        if (working_list.Count <= 1) 
+        {
+            return working_list;
+        }
+        List<int> left = new List<int>();
+        List<int> right = new List<int>();
+
+        for (int index = 0; index <= working_list.Count - 1; index++)
+        {
+            if (index < (working_list.Count / 2))
+            {
+                left.Add(working_list[index]);
+            }
+            else
+            {
+                right.Add(working_list[index]);
+            }
+        }
+        left = Merge(left);
+        right = Merge(right);
+
+        return Merge_Sublists(left, right);
+    }
+    private List<int> Merge_Sublists(List<int> left, List<int> right)
+    {
+        List<int> result = new List<int>();
+
+        while (left.Count > 0 & right.Count > 0)
+        {
+            if (left[0] <= right[0])
+            {
+                result.Add(left[0]);
+                left.RemoveAt(0);
+            }
+            else
+            {
+                result.Add(right[0]);
+                right.RemoveAt(0);
+            }
+        }
+        while (left.Count > 0)
+        {
+            result.Add(left[0]);
+            left.RemoveAt(0);
+        }
+        while (right.Count > 0)
+        {
+            result.Add(right[0]);
+            right.RemoveAt(0);
+        }
+
+        return result;
     }
 }
